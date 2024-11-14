e4f4e5d31b916eb3ab2bebbab46ad82443b77904
hs-44 <harshvardhansutar4444@gmail.com>
2024-11-15 02:01:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,18 +2,14 @@
 
 resource "aws_vpc" "terraform_test_vpc" {
   cidr_block           = "172.31.0.0/16"
-<<<<<<< HEAD
+
   enable_dns_hostnames = false
   enable_dns_support   = false
-=======
   enable_dns_support   = true
-=======
-
   enable_dns_hostnames = true
 
 
 
->>>>>>> e22f4764cf3dee9fd473c0d5fcf5d7fa66d61139
 
 
   tags = {
