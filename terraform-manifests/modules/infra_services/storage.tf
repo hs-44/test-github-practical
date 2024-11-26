8bb0ec7d6d01988a829ad44bd66736e32b40eb0f
hs-44 <harshvardhansutar4444@gmail.com>
2024-11-26 15:53:52 +0530

    Update storage.tf

--- terraform-manifests/modules/infra_services/storage.tf
+++ terraform-manifests/modules/infra_services/storage.tf
@@ -1,14 +1,14 @@
-resource "aws_s3_bucket" "s3_bucket" {
+resource "nks_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
 }
 
-# resource "aws_s3_bucket_acl" "s3_bucket_acl" {
-#   bucket = aws_s3_bucket.s3_bucket.id
+# resource "nks_s3_bucket_acl" "s3_bucket_acl" {
+#   bucket = nks_s3_bucket.s3_bucket.id
 #   acl    = var.bucket_acl
 # }
 
-resource "aws_s3_bucket_versioning" "s3_versioning" {
-  bucket = aws_s3_bucket.s3_bucket.id
+resource "nks_s3_bucket_versioning" "s3_versioning" {
+  bucket = nks_s3_bucket.s3_bucket.id
   versioning_configuration {
     status = "Enabled"
   }
